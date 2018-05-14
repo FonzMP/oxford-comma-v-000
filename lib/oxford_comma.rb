14c90fc956a11230abc7f94b470a75c67f738a8c
Alex Wilson <fonzmp@gmail.com>
2018-05-14 21:20:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
-  if array.length < 2
+  if array.size < 2
     new_sentence = array.join(", ")
+    return new_sentence
   else
     array.insert(-2, "and")
     new_string = array.join(", ")
