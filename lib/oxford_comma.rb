c065077e4cc378cc19997bb768427dc682a29a09
Alex Wilson <fonzmp@gmail.com>
2018-05-14 21:32:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     return new_sentence
   elsif length == 2
     new_sentance = array[0] + " and " array[1]
-    return new_sentance
+    return new_sentence
   else
     new_string = array[0..-2].join(", ") + " and " + array[-1]
     return new_string
